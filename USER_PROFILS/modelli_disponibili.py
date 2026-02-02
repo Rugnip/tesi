f03e4d2f291ddf16610c10669be5e4923875e829
Rugnip <146069358+Rugnip@users.noreply.github.com>
2026-02-02 13:33:15 +0100

    Update modelli_disponibili.py

--- USER_PROFILS/modelli_disponibili.py
+++ USER_PROFILS/modelli_disponibili.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyCpgWjNDycIZTHKRI7z8mR54KA26pE8Sh0")
+client = genai.Client(api_key="")
 
 for model in client.models.list():
     print(model.name)
